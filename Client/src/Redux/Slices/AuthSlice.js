9d713da817499bbe83bd2ac048dfc832546d3ffd
SalmaFennan <salmafennan19@gmail.com>
2025-08-23 20:02:10 +0100

    Corriger le chemin d'import pour Helpers/axiosInstance

--- Client/src/Redux/Slices/AuthSlice.js
+++ Client/src/Redux/Slices/AuthSlice.js
@@ -1,7 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import toast from "react-hot-toast";
 import axiosInstance from "../../Helpers/axiosInstance";
-
 const isBrowser = typeof window !== "undefined"; 
 
 const safeParse = (value, fallback) => {
